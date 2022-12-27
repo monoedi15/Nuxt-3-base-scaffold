3bc5303c20584c4505b7caf114b78d26af6fe918
[monoedi15] <[Your>
2022-12-27 01:53:18 -0500

    editor config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-    'plugin:vue/vue3-essential',
-    'plugin:nuxt/recommended',
-    'prettier',
-  ],
+  extends: ['airbnb-base', 'plugin:vue/vue3-essential', 'plugin:nuxt/recommended', 'prettier'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
